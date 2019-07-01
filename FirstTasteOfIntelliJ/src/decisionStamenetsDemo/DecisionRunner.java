6f4feee373d10b1f7ca561dfa9f5859a46664804
Tunde Faneye <tunde_eye@yahoo.com>
2019-07-01 21:48:04 +0100

    tt

---
+++ FirstTasteOfIntelliJ/src/decisionStamenetsDemo/DecisionRunner.java
@@ -0,0 +1,11 @@
+package decisionStamenetsDemo;
+
+public class DecisionRunner {
+
+    public static void main(String[] args) {
+
+        Vehicle vehicle = new Vehicle();
+        //vehicle.applyBrakes(45.0);
+        vehicle.checkYourGrade();
+    }
+}
